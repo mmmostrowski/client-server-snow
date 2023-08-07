3ad6e2b727be686d1b3d1d509dc41ea3f5298705
Maciej Ostrowski <mmm.ostrowski@gmail.com>
2023-08-07 21:09:33 +0200

    Reduce docker image sizes by a half

--- docker/java-proxy/entrypoint.sh
+++ docker/java-proxy/entrypoint.sh
@@ -27,7 +27,7 @@ function main() {
             mkdir -p "/snow/.pipes/";
             cd /snow/java-proxy/
             if [[ ! -e /snow/java-proxy/build/libs/proxy-0.0.1-SNAPSHOT.jar ]] || [[ ! -e  /data/app-gradle-build/ ]]; then
-                gradle bootJar --project-dir /snow/java-proxy/
+                gradle --no-daemon  bootJar --project-dir /snow/java-proxy/
             fi
             java -jar /snow/java-proxy/build/libs/proxy-0.0.1-SNAPSHOT.jar
         )
